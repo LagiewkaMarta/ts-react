1ec5a0c1578eecc29779d1e1e06e9bccb83a8c0d
Marta <lagiewka.marta.dev@gmail.com>
2019-10-14 23:59:47 +0200

    example 4 - onChange events

--- src/3.hooks/index.tsx
+++ src/3.hooks/index.tsx
@@ -1,11 +1,27 @@
 import * as React from "react";
 
-export const Ex3:React.FC = () => {
+interface Color {
+    color?: string,
+}
+
+interface Props {
+    onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+    value: string
+}
+
+export const Ex3:React.FC<Props>= (props) => {
     const [count, setCount] = React.useState<number | null>(8);
     //the same with objects 
-    const [color, setColor] = React.useState<{color: string}>({color: "red"});
-    setCount(null);
+    const [color, setColor] = React.useState<Color>({color: "red"});
+    
+    //useRef
+    //hover over the squigglies in ref to see what type we can pass 
+    //in case of ref it's <HTMLInputElement>
+    const inputRef = React.useRef<HTMLInputElement>(null);
+    // setCount(null);
     return (
-        <p></p>
+        //hover over onChange to see what type should be passed from props
+       <input value={props.value} ref={inputRef} type="text" onChange={(e) => props.onInputChange(e)}/>
+
     )
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import './App.css';
 import { Ex2 } from './2.class-component';
+import { Ex3 } from './3.hooks';
 // import { Ex1 } from './1.functional-component';
 
 const foo = () => {
 	console.log('foo');
 };
 
-const App: React.FC = () => {
-	return (
-		<>
-			{/* <Ex1 fn={foo} name="hh" ok={false}></Ex1> */}
-			<Ex2 num={9}></Ex2>
-		</>
-	);
+class App extends React.Component {
+  state = {
+    value: ""
+  }
+
+  // onChane events 
+  onInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  render(){
+    return (
+      <>
+        {/* <Ex1 fn={foo} name="hh" ok={false}></Ex1> */}
+        {/* <Ex2 num={9}></Ex2> */}
+        <Ex3 onInputChange={this.onInputChange} value={this.state.value}/>
+      </>
+    );
+  }
+
 };
 
 export default App;
